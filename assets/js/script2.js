5d7554126dbe243df6aaddaf80040e692345a3c3
Sean Collins <iseanc@hotmail.com>
2022-10-26 13:18:56 -0700

    updated URL var with more search criteria.  Updated getYouTubeContent so it loads search result into an array, randomly sorts the array, loads the playlist, and starts playing it.  NOTE: some videos seem to pause a second after they start playing.  Just hit play to keep playing.

---
+++ assets/js/script2.js
@@ -0,0 +1,67 @@
+var moodForm = document.getElementById("mood-form");
+var moodSelection = document.getElementById("current-mood");
+var moodButton = document.getElementById("mood-button");
+var videoList = document.getElementById("video-list");
+var songList = document.getElementById("song-list");
+var movieList = document.getElementById("movie-list");
+
+
+
+
+//Youtube API player code
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+function onYouTubeIframeAPIReady() {
+    
+  player = new YT.Player('player', {
+    height: '390',
+    width: '640',
+    videoId: 'M7lc1UVf-VE',
+    playerVars: {
+      'playsinline': 1
+    },
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}
+
+function onPlayerReady(event) {
+    event.target.playVideo();
+  }
+
+  var done = false;
+  function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+      setTimeout(stopVideo, 6000);
+      done = true;
+    }
+  }
+  function stopVideo() {
+    player.stopVideo();
+  }
+
+  var player;
+  function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+      height: '390',
+      width: '640',
+      videoId: 'M7lc1UVf-VE',
+      playerVars: {
+        'playsinline': 1
+      },
+      events: {
+        'onReady': onPlayerReady,
+        'onStateChange': onPlayerStateChange
+      }
+    });
+  }
+//End of youtube API code
+
+
