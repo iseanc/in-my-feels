38ead8505b64e08abc28413ffb3b02eba7d3a768
maggierdelaney <111933814+maggierdelaney@users.noreply.github.com>
2022-10-29 18:07:14 -0500

    css stlying and readme

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,112 +16,79 @@ var arrayYTVideoIds = [];
 
 var savedContent = [];
 
-// ***** BEGIN: THIS SECTION IS DUPLICATE OF CODE DEFINED AGAIN BELOW
-// gets the selected mood value and executes content search (video)
-// moodForm.addEventListener("submit", (event) => {
-//   var moodSelection = document.getElementById("mood-selections");
-//   var value = moodSelection.value;
-//   var text = moodSelection.options[moodSelection.selectedIndex].text;
-//   console.log("hello");
-//   console.log(value);
-//   console.log(text);
-//   event.preventDefault();
-//   getYoutubeContent(value);
-// });
-
-//fetch search content from YouTube.
-// function getYoutubeContent(value) {
-//   var queryUrl = `https://www.googleapis.com/youtube/v3/search?key=${ytAPIkey}&q=${value}&videoEmbeddable=${videoEmbed}&videoLicense=${videoLicenseType}&type=${ytSearchType}&maxResults=${maxResults}`;
-//   fetch(queryUrl)
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (data) {
-//       console.log(data);
-//       getFeelsResultYT = data;
-//       for (var i = 0; i < getFeelsResultYT.items.length; i++) { 
-//         var vid = getFeelsResultYT.items[i]; 
-//         console.log("vidID:", vid.id.videoId);
-//         arrayYTVideoIds.push(vid.id.videoId);
-//       }
-//       arrayYTVideoIds = [...arrayYTVideoIds].sort(() => 0.5 - Math.random());
-//       console.log("arrayYTVideoIds", arrayYTVideoIds);
-//       player.loadPlaylist(arrayYTVideoIds);
-//     });
-// };
-// ***** END: THIS SECTION IS DUPLICATE OF CODE DEFINED AGAIN BELOW
-
 // gets the selected mood value and executes content search (video)
 moodForm.addEventListener("submit", (event) => {
-  var moodSelection = document.getElementById("mood-selections");
-  var value = moodSelection.value;
-  var text = moodSelection.options[moodSelection.selectedIndex].text;
-  console.log("hello");
-  console.log(value);
-  console.log(text);
-  event.preventDefault();
-  // fetch YouTube content
-  getYoutubeContent(value);
-  arrayYTVideoIds = [];
+    var moodSelection = document.getElementById("mood-selections");
+    var value = moodSelection.value;
+    var text = moodSelection.options[moodSelection.selectedIndex].text;
+    console.log("hello");
+    console.log(value);
+    console.log(text);
+    event.preventDefault();
+    // fetch YouTube content
+    getYoutubeContent(value);
+    arrayYTVideoIds = [];
 
-  // fetch Mixcloud content
-  getMixcloudContent(value)
+    // fetch Mixcloud content
+    getMixcloudContent(value)
 });
 
 // fetch content from youtube
 function getYoutubeContent(value) {
-  var queryUrl = `https://www.googleapis.com/youtube/v3/search?key=${ytAPIkey}&q=${value}&videoEmbeddable=${videoEmbed}&videoLicense=${videoLicenseType}&type=${ytSearchType}&maxResults=${maxResults}`;
-  fetch(queryUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-      getFeelsResultYT = data;
-      for (var i = 0; i < getFeelsResultYT.items.length; i++) { 
-        var vid = getFeelsResultYT.items[i]; 
-        console.log("vidID:", vid.id.videoId);
-        arrayYTVideoIds.push(vid.id.videoId);
-      }
-      arrayYTVideoIds = [...arrayYTVideoIds].sort(() => 0.5 - Math.random());
-      console.log("arrayYTVideoIds", arrayYTVideoIds);
-      player.loadPlaylist(arrayYTVideoIds);
-      localStorage.setItem("saved-content", JSON.stringify(arrayYTVideoIds));
-      var example = localStorage.getItem("saved-content");
-      console.log(example);
-      var savedContent = (text) => {
-        var savedButton = document.createElement("button");
-        savedButton.setAttribute("data-playlist", text);
-        savedButton.innerHTML = "Last Playlist";
-        savedContentList.appendChild(savedButton);
-        savedButton.addEventListener('click', function () {
-            getYoutubeContent2(this.getAttribute("data-playlist"));
-            console.log(savedButton);
+    var queryUrl = `https://www.googleapis.com/youtube/v3/search?key=${ytAPIkey}&q=${value}&videoEmbeddable=${videoEmbed}&videoLicense=${videoLicenseType}&type=${ytSearchType}&maxResults=${maxResults}`;
+    fetch(queryUrl)
+        .then(function (response) {
+            return response.json();
         })
-    };
-    savedContent(example);
-    });
+        .then(function (data) {
+            console.log(data);
+            getFeelsResultYT = data;
+            for (var i = 0; i < getFeelsResultYT.items.length; i++) {
+                var vid = getFeelsResultYT.items[i];
+                console.log("vidID:", vid.id.videoId);
+                arrayYTVideoIds.push(vid.id.videoId);
+            }
+            arrayYTVideoIds = [...arrayYTVideoIds].sort(() => 0.5 - Math.random());
+            console.log("arrayYTVideoIds", arrayYTVideoIds);
+            player.loadPlaylist(arrayYTVideoIds);
+            localStorage.setItem("saved-content", JSON.stringify(arrayYTVideoIds));
+            var example = localStorage.getItem("saved-content");
+            console.log(example);
+            var savedContent = (text) => {
+                var savedButton = document.createElement("button");
+                savedButton.setAttribute("data-playlist", text);
+                savedButton.innerHTML = "Last Playlist";
+                savedContentList.appendChild(savedButton);
+                savedButton.addEventListener('click', function () {
+                    alert(this.getAttribute("data-playlist"));
+                    getYoutubeContent2(this.getAttribute("data-playlist"));
+                    console.log(savedButton);
+                })
+            };
+            savedContent(example);
+        });
 };
 
 // fetch youtube content when selecting to view from previous selections
 function getYoutubeContent2(value) {
     var queryUrl = `https://www.googleapis.com/youtube/v3/search?key=${ytAPIkey}&q=${value}&videoEmbeddable=${videoEmbed}&videoLicense=${videoLicenseType}&type=${ytSearchType}&maxResults=${maxResults}`;
     fetch(queryUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data);
-        getFeelsResultYT = data;
-        for (var i = 0; i < getFeelsResultYT.items.length; i++) { 
-          var vid = getFeelsResultYT.items[i]; 
-          console.log("vidID:", vid.id.videoId);
-          arrayYTVideoIds.push(vid.id.videoId);
-        }
-        console.log("arrayYTVideoIds", arrayYTVideoIds);
-        player.loadPlaylist(arrayYTVideoIds);
-      });
-  };
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            alert(value);
+            getFeelsResultYT = data;
+            for (var i = 0; i < getFeelsResultYT.items.length; i++) {
+                var vid = getFeelsResultYT.items[i];
+                console.log("vidID:", vid.id.videoId);
+                arrayYTVideoIds.push(vid.id.videoId);
+            }
+            console.log("arrayYTVideoIds", arrayYTVideoIds);
+            player.loadPlaylist(arrayYTVideoIds);
+        });
+};
 
 //Youtube API player code
 var tag = document.createElement('script');
@@ -136,32 +103,47 @@ function onPlayerReady(event) {
 // plays video for 6 seconds, then stops
 var done = false;
 function onPlayerStateChange(event) {
-  if (event.data == YT.PlayerState.PLAYING && !done) {
-    setTimeout(stopVideo, 1000);
-    done = true;
-  }
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+        setTimeout(stopVideo, 1000);
+        done = true;
+    }
 }
 // stop video playback
 function stopVideo() {
-  player.stopVideo();
+    player.stopVideo();
 }
 
 var player;
 // creates a new YouTube video viewer
 function onYouTubeIframeAPIReady() {
-  player = new YT.Player('player', {
-    height: '390',
-    width: '640',
-    videoId: initialVideoID,
-    playerVars: {
-      'playsinline': 1
-    },
-    events: {
-      'onReady': onPlayerReady,
-      'onStateChange': onPlayerStateChange
-    }
-  });
+    player = new YT.Player('player', {
+        height: '390',
+        width: '640',
+        videoId: initialVideoID,
+        playerVars: {
+            'playsinline': 1
+        },
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
 }
+
+// function onYouTubePlayerAPIReady(arrayYTVideoIds) 
+// {
+//         player = new YT.Player('player', 
+//         {
+//           height: '390',
+//           width: '640',
+//           playerVars: 
+//           {
+//             listType:'playlist',
+//             list: arrayYTVideoIds
+//           }
+//         });
+// }
+
 //End of youtube API code
 
 // ****************************************************************
@@ -175,57 +157,57 @@ var mixcloudPlayerEl = document.getElementById('mixcloud-player');
 var getMixcloudResults = {};
 var arrayMixcloudKeys = [];
 // add FEED (Key = /user/mix) from KEY value
-var mixcloudPlayUrl ="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed="
+var mixcloudPlayUrl = "https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed="
 
 
 var mixcloudButtonEl = document.getElementById("mx-next-button");
 
 
 mixcloudButtonEl.addEventListener("click", (event) => {
-  loadMixcloudSelection(arrayMixcloudKeys);
+    loadMixcloudSelection(arrayMixcloudKeys);
 });
 
 // mixcloud web fetch
 function getMixcloudContent(value) {
-  console.log("mixcloud VALUE passed:", value)
-  var queryUrl = `https://api.mixcloud.com/discover/${value}/latest/`;
+    console.log("mixcloud VALUE passed:", value)
+    var queryUrl = `https://api.mixcloud.com/discover/${value}/latest/`;
 
-  fetch(queryUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
+    fetch(queryUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
 
-      getMixcloudResults = data.data;
-      console.log("mixCloud", getMixcloudResults)
+            getMixcloudResults = data.data;
+            console.log("mixCloud", getMixcloudResults)
 
-      //clear array of video keys to avoid duplications, etc
-      arrayMixcloudKeys = [];
+            //clear array of video keys to avoid duplications, etc
+            arrayMixcloudKeys = [];
 
-      for (var i = 0; i < getMixcloudResults.length; i++) { 
-        var key = getMixcloudResults[i].key; 
-        console.log("key:", key);
-        arrayMixcloudKeys.push(key);
-      }
-      arrayMixcloudKeys = [...arrayMixcloudKeys].sort(() => 0.5 - Math.random());
-      console.log("arrayMixcloudKeys", arrayMixcloudKeys);
-      // player.loadPlaylist(arrayYTVideoIds);
+            for (var i = 0; i < getMixcloudResults.length; i++) {
+                var key = getMixcloudResults[i].key;
+                console.log("key:", key);
+                arrayMixcloudKeys.push(key);
+            }
+            arrayMixcloudKeys = [...arrayMixcloudKeys].sort(() => 0.5 - Math.random());
+            console.log("arrayMixcloudKeys", arrayMixcloudKeys);
+            // player.loadPlaylist(arrayYTVideoIds);
 
-      // load new Mixcloud selection
-      loadMixcloudSelection(arrayMixcloudKeys)
-    });
+            // load new Mixcloud selection
+            loadMixcloudSelection(arrayMixcloudKeys)
+        });
 };
 
 function loadMixcloudSelection(arrayOfTracks) {
 
     // get first element and remove it from the arrayOfTracks
     var track = arrayOfTracks.shift();
-    
+
     console.log("track", track);
     console.log("arrayOfTracks", arrayOfTracks);
     console.log("trackUrl", trackUrl);
-    
+
     //drop any existing widget from the mixcloudPlayerEl parent
     var currentIFrame = document.getElementById('mx-iframe');
     mixcloudPlayerEl.removeChild(currentIFrame);
@@ -238,7 +220,7 @@ function loadMixcloudSelection(arrayOfTracks) {
     var widgetBorder = "0";
     var widgetIFrame = document.createElement("iframe");
     widgetIFrame.id = widgetId;
-    widgetIFrame.width = widgetWidth; 
+    widgetIFrame.width = widgetWidth;
     widgetIFrame.height = widgetHeight;
     widgetIFrame.setAttribute("border", "0");
     widgetIFrame.src = widgetSrc;
